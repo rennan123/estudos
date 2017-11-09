63d86c8645184a54e36f14f1160d1130dcee9f6e
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2017-11-09 20:49:39 -0200

    inferencia de tipos

---
+++ angular/typescript/app.ts
@@ -0,0 +1,5 @@
+let mensagem: string = "seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let listaDeFrutas: Array<string> = ['uva', 'banana', 'abacate']
+let notaDasProvas: Array<number> = [7.5, 8, 9]
---
+++ angular/typescript/dist/app.js
@@ -0,0 +1,5 @@
+let mensagem = "seja bem vindo!!!"
+let temporadasFriends = 10
+let estudandoAngular = true
+let listaDeFrutas = ['uva', 'banana', 'abacate']
+let notaDasProvas = [7.5, 8, 9]
