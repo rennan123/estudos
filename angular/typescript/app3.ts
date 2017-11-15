cd088e1b477215dddd7042c1248b2a78323d99c6
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2017-11-15 13:45:56 -0200

    herança

---
+++ angular/typescript/app3.ts
@@ -0,0 +1,13 @@
+import Carro from './Carro'
+import Moto from './Moto'
+
+let carro = new Carro('veloster', 3);
+
+
+let moto = new Moto();
+moto.acelerar()
+moto.acelerar()
+carro.acelerar()
+
+console.log(moto)
+console.log(carro)
---
+++ angular/typescript/dist/app3.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Carro_1 = require("./Carro");
+var Moto_1 = require("./Moto");
+var carro = new Carro_1.default('veloster', 3);
+var moto = new Moto_1.default();
+moto.acelerar();
+moto.acelerar();
+carro.acelerar();
+console.log(moto);
+console.log(carro);
