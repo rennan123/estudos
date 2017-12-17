ef113e7907a6096dcee51211288107860c1fd4e5
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2017-12-17 16:21:36 -0200

    lógica do componente tentativas funcionando!

--- angular/app1/src/app/painel/painel.component.ts
+++ angular/app1/src/app/painel/painel.component.ts
@@ -12,16 +12,17 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
-  public resposta: string
+  public resposta: string = ''
 
   public rodada: number = 0
   public rodadaFrase: Frase
 
   public progresso: number = 0
 
+  public tentativas: number = 3
+
   constructor() { 
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase) 
+    this.atualizaRodada()
   }
 
   ngOnInit() {
@@ -33,7 +34,6 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta(): void {
-    
     if(this.rodadaFrase.frasePtBr == this.resposta) {
       alert('A tradução está correta')
 
@@ -42,16 +42,27 @@ export class PainelComponent implements OnInit {
 
       //progresso
       this.progresso = this.progresso + (100 / this.frases.length)
-      console.log(this.progresso)
 
       //atualiza o objeto rodadaFrase 
-      this.rodadaFrase = this.frases[this.rodada]
+      this.atualizaRodada()
 
     } else {
-      alert('A tradução está errada')
-    }
+      //diminuir a variável tentativas
+      this.tentativas--
 
-    
+      if(this.tentativas === -1) {
+        alert('Você perdeu todas as tentativas')
+      }
+    }
+  }
+
+  public atualizaRodada(): void {
+
+    //define a frase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada]
+
+    //limpar a resposta
+    this.resposta = ''
   }
 
 }
--- angular/app1/src/app/tentativas/tentativas.component.ts
+++ angular/app1/src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+
+import { Coracao } from '../shared/coracao.model'
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio: string = '/assets/coracao_vazio.png'
-  public coracaoCheio: string = '/assets/coracao_cheio.png'
+  @Input() public tentativas: number
 
-  constructor() { }
+  public coracoes: Coracao[] = [
+    new Coracao(true), new Coracao(true), new Coracao(true)
+  ]
+
+  constructor() { 
+    console.log(this.coracoes)
+  }
+
+  ngOnChanges() {
+    //this.tentativas
+    //this.coracoes.length
+    if(this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas
+
+      this.coracoes[indice - 1].cheio = false
+    }
+    console.log('tentativas recebidas do painel: ', this.tentativas)
+  }
 
   ngOnInit() {
+    
   }
 
 }
