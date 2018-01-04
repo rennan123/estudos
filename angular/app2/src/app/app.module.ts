158f95b01b3b75a6a0c7d359d65c520948c4e848
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2018-01-04 20:31:23 -0200

    aplicação passaro urbano iniciada

---
+++ angular/app2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
