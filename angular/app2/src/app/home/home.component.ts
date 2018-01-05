fbb037ca43266d1a1a39c66c2634d3c3d820aac5
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2018-01-05 00:57:13 -0200

    rotas configuradas

--- angular/app2/src/app/home/home.component.ts
+++ angular/app2/src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
 
     this.ofertasService.getOfertas()
       .then(( ofertas: Oferta[] ) => { 
-        this.ofertas = ofertas;
+        this.ofertas = ofertas 
       })
       .catch(( param: any ) => { 
         console.log(param) 
