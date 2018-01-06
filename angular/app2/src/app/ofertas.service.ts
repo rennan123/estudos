a718dd17f6981ccef799f8d0beb3d63700501f2f
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2018-01-06 16:20:39 -0200

    componente oferta configurado

--- angular/app2/src/app/ofertas.service.ts
+++ angular/app2/src/app/ofertas.service.ts
@@ -20,4 +20,12 @@ export class OfertasService {
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                return resposta.json()[0]
+            })
+    }
 }
