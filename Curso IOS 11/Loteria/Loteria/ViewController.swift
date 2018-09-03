ddfe912aab9e60f3540dfb7aa055ffd5ddfc80e0
Rennan Oliveira <rennan.h.oliveira@gmail.com>
2018-09-03 15:51:06 -0300

    app loteria iniciado e layout feito

---
+++ Curso IOS 11/Loteria/Loteria/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Rennan Oliveira on 03/09/18.
+//  Copyright © 2018 Rennan Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func generateGame() {
+        
+        
+        
+    }
+    
+}
+
